33c0fbd151008766706508feabd04fa7bb3ecaa4
Abhinav S Vijayan <abhinavsvijayan15@gmail.com>
2025-07-09 22:49:23 +0530

    Add files via upload

---
+++ Day1_Variables.js
@@ -0,0 +1,21 @@
+//*SECTION 1: Creating Variables
+
+let num = 123               //variables in js is created using let,const
+const city = "Chicago"      //const is used to declare variables whose values cannot change later
+let job = "Plumber"        //let is used to declare variables whose values can change later
+
+
+
+//*SECTION 2: Displaying in console
+
+console.log(num)                      //console.log is used to print values to the console
+console.log(city,num)                //we can print multiple variables in the same line
+
+let Activity = "Coding"  
+console.log(Activity)     
+Activity = "Reading"      //we can change the value of a variable declared with let
+console.log(Activity)     //when we update a variable, it forgets its previous value
+
+let con = "Following:" + 500
+console.log(con)       //("Followers: " + 1000) we can concatenate strings and numbers using the + operator
+
