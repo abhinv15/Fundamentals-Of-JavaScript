95aeab7a9a57a4534b0a9e79a9cf9f93f17bf292
Abhinav S Vijayan <abhinavsvijayan15@gmail.com>
2025-07-10 22:49:04 +0530

    Add files via upload

---
+++ Day2_Operators.js
@@ -0,0 +1,35 @@
+                                 // Learning Operators
+
+//*SECTION 1: Arithmetic Operators 
+const a = 10
+const b = 20
+console.log(a + b) //addition
+console.log(a - b) //subtraction    
+console.log(a * b) //multiplication
+console.log(a / b) //division
+
+
+//*SECTION 2: Comparison Operators
+console.log(a < b)    //less - than operator : checks whether a number is less than another number
+console.log(b > a)  //greater - than operator : checks whether a number is greater than another number
+
+console.log(10 <= 20) //less than or equal to operator : checks whether a number is less than or equal to another number
+console.log(20 >= 10) //greater than or equal to operator : checks whether a number is greater than or equal to another number
+
+
+//*SECTION 3: Boolean Operators
+console.log("Device Charged")
+let status = true
+console.log(status)    //true or false values are called boolean values
+status = false
+console.log(!true)     //the ! operator negates the value, so !true is false
+
+
+//*SECTION 4: Equality Operators
+console.log(10 == 10)             //== or === checks for value equality, it returns either true or false
+console.log("apple" == "apple") 
+
+
+//*SECTION 5:Inequality Operators
+console.log(10 != 20)             //!= or !== checks for value inequality, it returns either true or false
+console.log("apple" != "banana") //it checks whether the two values are not equal, it returns either true or false
